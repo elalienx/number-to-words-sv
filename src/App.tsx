6f4cfcb799c4768a17cc0017f87d0184a3e47b4e
Eduardo Alvarez <el_alienx@hotmail.com>
2025-03-10 13:22:04 +0100

    adding placeholders

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+// Node modules
+import { useState } from "react";
+
 export default function App() {
+  // Local state
+  const [value, setValue] = useState(0);
+  const [result, setResult] = useState("zero");
+
+  function onChage(event: React.ChangeEvent<HTMLInputElement>) {
+    const newValue: number = Number(event.target.value);
+    const newResult: string = numberToWordsSV(newValue);
+
+    setValue(newValue);
+    setResult(newResult);
+  }
+
   return (
     <div>
       <h1>Number to words, Swedish</h1>
+      <input type="number" value={value} onChange={(event) => onChage(event)} />
+      <p>Your value is: {result}</p>
     </div>
   );
 }
