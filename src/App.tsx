ba08a5aa4844f9c3357318a867994cd257fcdd9c
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2025-03-11 09:36:58 +0100

    small refactor

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,14 @@ import numberToWordsSV from "./scripts/numberToWordsSV";
 export default function App() {
   // Local state
   const [value, setValue] = useState(0);
-  const [result, setResult] = useState("zero");
+  const [result, setResult] = useState(numberToWordsSV(value));
 
-  function onChage(event: React.ChangeEvent<HTMLInputElement>) {
+  // Properties
+  const MINIMUM_VALUE = 0;
+  const MAXIMUM_VALUE = 100_000;
+
+  // Methods
+  function onInputChange(event: React.ChangeEvent<HTMLInputElement>) {
     const newValue: number = Number(event.target.value);
     const newResult: string = numberToWordsSV(newValue);
 
@@ -21,12 +26,11 @@ export default function App() {
     <div>
       <h1>Number to words, Swedish</h1>
       <input
+        max={MAXIMUM_VALUE}
+        min={MINIMUM_VALUE}
+        onChange={(event) => onInputChange(event)}
         type="number"
         value={value}
-        onChange={(event) => onChage(event)}
-        placeholder="100 000"
-        min={0}
-        max={100000000}
       />
       <p>
         Du har angett <b>{result}</b>, är det korrekt?
--- src/scripts/tens.ts
+++ src/scripts/tens.ts
@@ -3,7 +3,7 @@ export default function tens(value: number): string {
   const digits = ["nul", "ett", "två", "tre", "fyra", "fem", "sex", "sju", "åtta", "nio", "tio"]
 
   // Safeguards
-  if (value < 0) return "Nummer is less than nul";
+  if (value < 0) return "Nummer is less than noll";
   if (value > 10) return "Nummer is bigger than tio";
 
   return digits[value];
