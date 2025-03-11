b456c40b33386b3a40e73897f1647d9b26f1c0ff
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2025-03-11 11:11:42 +0100

    adding tests for bigger numbers

---
+++ src/scripts/numberToWordsSV.test.ts
@@ -0,0 +1,57 @@
+// Node modules
+import { describe, expect, test } from "vitest";
+
+// Project files
+import numberToWordsSV from "./numberToWordsSV";
+
+describe("Edge cases", () => {
+  test("Expect 0 to be noll", () => {
+    // Arrange
+    const value = 0;
+    const result = "noll";
+
+    // Act
+    const test = numberToWordsSV(value);
+
+    // Assert
+    expect(test).toBe(result);
+  });
+
+  test("Expect 1_000_000_000_000_000 to return error message", () => {
+    // Arrange
+    const value = 1_000_000_000_000_000;
+    const result = "Nummer är större än gränsen";
+
+    // Act
+    const test = numberToWordsSV(value);
+
+    // Assert
+    expect(test).toBe(result);
+  });
+});
+
+describe("Normal cases", () => {
+  test("Expect 300_000 to be ett", () => {
+    // Arrange
+    const value = 300_000;
+    const result = "trehundra tusen";
+
+    // Act
+    const test = numberToWordsSV(value);
+
+    // Assert
+    expect(test).toBe(result);
+  });
+
+  test("Expect 1_000_000 to be niohundra nittio nio", () => {
+    // Arrange
+    const value = 1_000_000;
+    const result = "ett miljon";
+
+    // Act
+    const test = numberToWordsSV(value);
+
+    // Assert
+    expect(test).toBe(result);
+  });
+});
--- src/scripts/numberToWordsSV.ts
+++ src/scripts/numberToWordsSV.ts
@@ -9,11 +9,11 @@ export default function numberToWordsSV(value: number): string {
   if (value < 0) return "Nummer är mindre än noll";
   if (value > 999_999_999_999_999) return "Nummer är större än gränsen";
 
-  let parts = [];
+  const parts = [];
   let thousandIndex = 0;
 
   while (value > 0) {
-    let chunk = value % 1000;
+    const chunk = value % 1000;
 
     if (chunk > 0) {
       let chunkWords = convertBelowThousand(chunk);
